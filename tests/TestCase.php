615065986361b827cfac7289b14629b3dac6bf1d
Vildan Bina <bexvibi@bexeo.com>
2020-04-23 15:26:50 +0200

    initial commit

---
+++ tests/TestCase.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace bexvibi\GeoIP\Tests;
+
+use Mockery;
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+class TestCase extends PHPUnitTestCase
+{
+    public static $functions;
+
+    public function setUp(): void
+    {
+        self::$functions = Mockery::mock();
+    }
+
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    protected function makeGeoIP(array $config = [], $cacheMock = null)
+    {
+        $cacheMock = $cacheMock ?: Mockery::mock('Illuminate\Cache\CacheManager');
+
+        $config = array_merge($this->getConfig(), $config);
+
+        $cacheMock->shouldReceive('tags')->with(['bexvibi-geoip-location'])->andReturnSelf();
+
+        return new \bexvibi\GeoIP\GeoIP($config, $cacheMock);
+    }
+
+    protected function getConfig()
+    {
+        $config = include(__DIR__ . '/../config/geoip.php');
+
+        $this->databaseCheck($config['services']['maxmind_database']['database_path']);
+
+        return $config;
+    }
+
+    /**
+     * Check for test database and make a copy of it
+     * if it does not exist.
+     *
+     * @param string $database
+     */
+    protected function databaseCheck($database)
+    {
+        if (file_exists($database) === false) {
+            @mkdir(dirname($database), 0755, true);
+            copy(__DIR__ . '/../resources/geoip.mmdb', $database);
+        }
+    }
+}
